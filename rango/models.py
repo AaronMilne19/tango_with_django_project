aeb58d3c5d51ad3ea89f50c474bddee83684ba7c
Aaron Milne <2466574M@student.gla.ac.uk>
2021-01-24 15:34:47 +0000

    End of 5.4

--- rango/models.py
+++ rango/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=true)
+    name = models.CharField(max_length=128, unique=True)
     
     def __str__(self):
         return self.name
 
-class Page(models.model):
+class Page(models.Model):
     category =models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
